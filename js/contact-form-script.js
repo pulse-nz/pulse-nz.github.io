f125c2f7a311eca619e15ce5dc4eeb6cd6bd6190
Craig Oliver <Craig.A.Oliver@gmail.com>
2018-05-19 23:57:06 +1200

    Contact form

--- js/contact-form-script.js
+++ js/contact-form-script.js
@@ -3,11 +3,7 @@ $("#contactForm").validator().on("submit", function (event) {
         // handle the invalid form...
         formError();
         submitMSG(false, "Did you fill in the form properly?");
-    } else {
-        // everything looks good!
-        event.preventDefault();
-        submitForm();
-    }
+      }
 });
 
 
@@ -15,14 +11,28 @@ function submitForm(){
     // Initiate Variables With Form Content
     var name = $("#name").val();
     var email = $("#email").val();
+    var phone = $("#phone").val();
     var msg_subject = $("#msg_subject").val();
     var message = $("#message").val();
 
 
     $.ajax({
         type: "POST",
-        url: "php/form-process.php",
-        data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&message=" + message,
+        dataType: "xml",
+        xhrFields: {
+          withCredentials: true
+        },
+        url: "https://docs.google.com/forms/d/e/1FAIpQLSdNaaFpUiE7CzLKyDJuWmxo0gY93wsY7QOOaeziMIdqoafoGQ/formResponse",
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader('Access-Control-Allow-Origin', 'chrome-extension://EXTENSION_ID');
+          xhr.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
+        },
+        data: {
+          "emailAddress": email,
+          "entry.2005620554": name,
+          "entry.1166974658": phone,
+          "entry.839337160" : message
+        },
         success : function(text){
             if (text == "success"){
                 formSuccess();
